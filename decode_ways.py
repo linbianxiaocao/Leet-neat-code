7efa8de317a1dd4e2b162fe5ea26d02558743467
Jie Yan <awesomejie@gmail.com>
2017-02-05 08:34:42 -0500

    dynamic programming for decode_ways

--- decode_ways.py
+++ decode_ways.py
@@ -11,33 +11,35 @@
 # 
 # The number of ways decoding "12" is 2.
 
-# http://yucoding.blogspot.ca/2013/01/leetcode-question-26-decode-ways.html
-# incomplete
+# http://bangbingsyb.blogspot.ca/2014/11/leetcode-decode-ways.html
+# f[n] = f[n-1] (if '1'<=s[n]<='9') + f[n-2] (if s[n-1]s[n] is betwen 10 and 26)
 class Solution(object):
     def numDecodings(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if not s:
+        if not s or s[0]<'1' or s[0]>'9':
             return 0
 
-        lres = []
-        if s[0] != 0:
-            lres.append(1)
-        else:
-            lres.append(0)
+        dp_l = [0]*(len(s)+1)
+        dp_l[0] = 1
+        dp_l[1] = 1
 
-        if len(s) == 1:
-            return lres[0]
+        for i in range(1, len(s)):
+            v1 = 0
+            v2 = 0
+            if not s[i].isdigit():
+                return 0
+            if s[i] != '0':
+                v1 = dp_l[i]
+            v_two_digits = (ord(s[i-1])-ord('0'))*10 + (ord(s[i])-ord('0'))
+            if v_two_digits >= 10 and v_two_digits <= 26:
+                v2 = dp_l[i-1]
+            dp_l[i+1] = v1 + v2
+            if dp_l[i+1] == 0:
+                return 0
 
-        if self.valid(s[0:2]):
-            lres.append(1)
-        
+        return dp_l[len(s)]
 
-    def valid(self, s):
-        if len(s) == 0:
-            return False
-        if s[0] == '0':
-            return False
 
