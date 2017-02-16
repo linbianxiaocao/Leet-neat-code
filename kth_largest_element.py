5d8e90352b3002cb65546983bb17727be206e8d1
Jie Yan <awesomejie@gmail.com>
2017-02-16 14:34:24 -0500

    finally get kth_largest_element.py right (based on idea of quick sort)

---
+++ kth_largest_element.py
@@ -0,0 +1,61 @@
+"""
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+"""
+import random
+
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+        q = self.randomPartition(nums)
+        ck = n-q-1
+        if ck >= k:
+            return self.findKthLargest(nums[q+1:], k)
+        elif ck < k:
+            return self.findKthLargest(nums[0:q+1], k-ck)
+
+    def randomPartition(self, nums):
+        """
+        modify nums and return q such that nums[0:q+1] <= pivot
+        and nums[q+1:] >= pivot
+        """
+        if len(nums) == 1:
+            return 0
+        ridx = random.randint(0, len(nums)-1)
+        # ridx = 0
+        pivot = nums[ridx]
+        nums[ridx] = nums[0]
+        nums[0] = pivot
+
+        i = 0
+        j = len(nums)-1
+        while True:
+            while nums[i] < pivot:
+                i += 1
+            while nums[j] > pivot:
+                j -= 1
+            if i < j:
+                tmp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = tmp
+                i += 1
+                j -= 1
+            else:
+                # print(nums)
+                return j
+
+s = Solution()
+print(s.randomPartition([7, 6, 5, 4, 3, 2, 1]))
---
+++ merge_k_sorted_lists.py
@@ -0,0 +1,63 @@
+"""
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+import sys
+
+
+class Solution(object):
+    # my own solution1: time limit exceeded
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        idx, smallest = -1, sys.maxsize
+        for i in range(len(lists)):
+            if lists[i] and lists[i].val < smallest:
+                idx, smallest = i, lists[i].val
+
+        if idx == -1:
+            return None
+
+        head = lists[idx]
+
+        lists[idx] = lists[idx].next
+        head.next = self.mergeKLists(lists)
+
+        return head
+
+    # my own solution2: use a min heap
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if len(lists) == 0:
+            return None
+
+        import heapq
+        h = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+
+        dummyhead = ListNode(0)
+        cnode = dummyhead
+        while len(h) != 0:
+            (val, idx) = heapq.heappop(h)
+            cnode.next = lists[idx]
+            cnode = cnode.next
+
+            lists[idx] = lists[idx].next
+            if lists[idx]:
+                heapq.heappush(h, (lists[idx].val, idx))
+
+        return dummyhead.next
