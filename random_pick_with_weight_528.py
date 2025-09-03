2ac136cfe4a0d946f85cbf614359194a4cb553a3
jAY <5757111+linbianxiaocao@users.noreply.github.com>
2025-09-03 11:47:38 -0700

    binary search for random pick with weight

--- random_pick_with_weight_528.py
+++ random_pick_with_weight_528.py
@@ -1,1 +1,16 @@
 https://blog.csdn.net/fuxuemingzhu/article/details/81807215
+
+import bisect
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.preSum = [0] * len(w)
+        self.preSum[0] = w[0]
+        for i in range(1, len(w)):
+            self.preSum[i] = self.preSum[i-1] + w[i]
+
+    def pickIndex(self) -> int:
+        total = self.preSum[-1]
+        rand = random.randint(1, total) # random int from [1, total]
+        ind = bisect.bisect_left(self.preSum, rand)
+        return ind
